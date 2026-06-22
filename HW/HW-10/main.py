773fd51f750bb27e1468fc5e76119062ed2622df
Zayans-World <zayanharifkp@gmail.com>
2026-06-22 15:41:51 +0530

    lolo

---
+++ HW/HW-10/main.py
@@ -0,0 +1,82 @@
+import cv2
+import numpy as np
+
+def apply_color_filter(image, filter_type):
+
+    """Apply the specified color filter to the image."""
+    filtered_image = image.copy()
+    if filter_type == 'red_tint':
+        filtered_image[:, :, 1] = 0
+        filtered_image[:, :, 0] = 0
+
+    elif filter_type == 'blue_tint':
+        filtered_image[:, :, 1] = 0
+        filtered_image[:, :, 2] = 0
+    
+    elif filter_type == 'green_tint':
+        filtered_image[:, :, 0] = 0
+        filtered_image[:, :, 2] = 0
+
+    elif filter_type == 'increase_red':
+        filtered_image[:, :, 2] = cv2.add(filtered_image[:, :, 2], 50)
+
+    elif filter_type == 'decrease_blue':
+        filtered_image[:, :, 0] = cv2.subtract(filtered_image[:, :, 0], 50)
+    
+    elif filter_type == 'gray_scale':
+        filtered_image = cv2.cvtColor(filtered_image, cv2.COLOR_BGR2GRAY)
+
+    return filtered_image
+
+image_path = input("Enter the path to the image: ")
+image = cv2.imread(image_path)
+
+if image is None:
+    print("Error: Image not found.")
+
+else :
+    filter_type = "original"
+
+    print("Press the following keys to apply filters:")
+    print("r - Red Tint")
+    print("b - Blue Tint")
+    print("g - Green Tint")
+    print("i - Increase Red")
+    print("d- Decrease Blue")
+    print("z - Gray Scale")
+    print("s - Save Filtered Image with a Custom Name")
+    print("q - Quit")
+
+    while True:
+        filtered_image = apply_color_filter(image, filter_type)
+        cv2.imshow("Filtered Image", filtered_image)
+        key = cv2.waitKey(0) & 0xFF
+
+        if key == ord('r'):
+            filter_type = 'red_tint'
+        elif key == ord('b'):
+            filter_type = 'blue_tint'
+        elif key == ord('g'):
+            filter_type = 'green_tint'
+        elif key == ord('i'):
+            filter_type = 'increase_red'
+        elif key == ord('d'):
+            filter_type = 'decrease_blue'
+        elif key == ord('z'):
+            filter_type = 'gray_scale'
+        elif key == ord('s'):
+            save_image_name = input("Enter the name for the saved image: ")
+            cv2.imwrite(save_image_name, filtered_image)
+            print(f"Image saved as {save_image_name}")
+            
+        elif key == ord('q'):
+            print("Exiting...")
+            break
+        else:
+            print("Invalid key. Please use 'r', 'b', 'g', 'i', 'd', or 'q'.")
+
+
+
+cv2.destroyAllWindows()
+        
+
